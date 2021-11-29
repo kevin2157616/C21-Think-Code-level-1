8f80c51ecd47c10f00783f240a931b9bd89e3225
kevin plas <kevinplas.rocva.nl>
2021-11-29 10:12:18 +0100

    opdracht 5 think code

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,9 @@
 #maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
 #elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
 #weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
+# Prints out the name óf å
+def favorite_city(name):
+    print("One of my favorite cities is", name
+favorite_city("Santa Barbara, California")
+favorite_city("Asheville, North Carolina")
+favorite_city("Amsterdam, The Netherlands")
